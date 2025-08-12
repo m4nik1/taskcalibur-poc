0a049e3655fe09ff142cf10bd11f944e66bfa20b
m4nik1 <maniksoomro@hotmail.com>
2025-08-12 00:06:53 -0400

    WIP Strikethrough on task when checked

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Task } from "../../types";
 import { formatTime } from "@/lib/utils"
 
@@ -10,59 +11,67 @@ interface TaskItemProps {
   index: number;
 }
 
-export default function TaskItem({ setDraggedTask, setDraggedTaskIndex, task, tasks, index, setTasks } : TaskItemProps) {
+export default function TaskItem({ setDraggedTask, setDraggedTaskIndex, task, tasks, index, setTasks }: TaskItemProps) {
+  const [completeCheck, setCheck] = useState(false);
+
+  function onTaskDrop(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    const dropIndex = Number.parseInt(
+      e.dataTransfer.getData("text/plain"),
+      10,
+    );
+    if (dropIndex != index) {
+      const newTasks = [...tasks];
+      const [movedTask] = newTasks.splice(dropIndex, 1);
+      newTasks.splice(index, 0, movedTask);
+      setTasks(newTasks);
+    }
+    setDraggedTaskIndex(null);
+  }
+
+  function completeTaskAction() {
     
+  }
+
   return (
-        <div
-            className="flex items-center gap-3 px-4 cursor-move
+    <div
+      className="flex items-center gap-3 px-4 cursor-move
             border-b border-gray-250 hover:bg-gray-50 dark:hover:bg-gray-800
             dark:border-gray-800"
-            style={{ height: "40px" }}
-            draggable
-            onDragStart={(e) => {
-              setDraggedTask(task.id);
-              setDraggedTaskIndex(index);
-              e.dataTransfer.effectAllowed = "move";
-              e.dataTransfer.setData("text/plain", index.toString());
-            }}
-            onDragOver={(e) => {
-              e.preventDefault();
-              e.dataTransfer.dropEffect = "move";
-            }}
-            onDrop={(e) => {
-              e.preventDefault();
-              const dropIndex = Number.parseInt(
-                e.dataTransfer.getData("text/plain"),
-                10,
-              );
-              if (dropIndex != index) {
-                const newTasks = [...tasks];
-                const [movedTask] = newTasks.splice(dropIndex, 1);
-                newTasks.splice(index, 0, movedTask);
-                setTasks(newTasks);
-              }
-              setDraggedTaskIndex(null);
-            }}
-            onDragEnd={() => {
-              setDraggedTask(null);
-              setDraggedTaskIndex(null);
-            }}
-          >
-            <input
-              type="checkbox"
-              className="w-3 h-3 border border-dashed border-gray-300 rounded-full flex-shrink-0"
-            />
-            <div className="flex-grow min-w-0">
-              <p className="text-sm font-medium text-gray-900 truncate">
-                {task.name}
-              </p>
-              <div className="flex items-center text-xs text-gray-500 mt-0.5">
-                <span>{formatTime(task.startHour)}</span>
-                <span className="mx-1">-</span>
-                <span>{formatTime(task.startHour + task.durationHours)}</span>
-              </div>
-            </div>
-            <hr className="my-12 h-0.5 border-t-0 bg-amber-400" />
-          </div>
-    )
+      style={{ height: "40px" }}
+      draggable
+      onDragStart={(e) => {
+        setDraggedTask(task.id);
+        setDraggedTaskIndex(index);
+        e.dataTransfer.effectAllowed = "move";
+        e.dataTransfer.setData("text/plain", index.toString());
+      }}
+      onDragOver={(e) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+      }}
+      onDrop={(e) => onTaskDrop(e)}
+      onDragEnd={() => {
+        setDraggedTask(null);
+        setDraggedTaskIndex(null);
+      }}
+    >
+      <input
+        type="checkbox"
+        onChange={(e) => setCheck(e.target.value)}
+        className="w-3 h-3 border border-dashed border-gray-300 rounded-full flex-shrink-0"
+      />
+      <div className="flex-grow min-w-0">
+        <p className={`text-sm font-medium text-gray-900 truncate ${completeCheck ? 'line-through' : ''}`}>
+          {task.name}
+        </p>
+        <div className="flex items-center text-xs text-gray-500 mt-0.5">
+          <span>{formatTime(task.startHour)}</span>
+          <span className="mx-1">-</span>
+          <span>{formatTime(task.startHour + task.durationHours)}</span>
+        </div>
+      </div>
+      <hr className="my-12 h-0.5 border-t-0 bg-amber-400" />
+    </div>
+  )
 }
