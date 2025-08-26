369a6fc1d6b52727f335b14cf4e4154d34b664ec
m4nik1 <maniksoomro@hotmail.com>
2025-08-26 08:36:03 -0400

    Commits to database backend

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { TaskDB } from "../../types";
-import { formatTime } from "@/lib/utils"
+import { formatTime } from "@/lib/utils";
 
 interface TaskItemProps {
   setDraggedTask: (id: number | null) => void;
@@ -11,35 +11,40 @@ interface TaskItemProps {
   index: number;
 }
 
-export default function TaskItem({ setDraggedTask, setDraggedTaskIndex, task, tasks, index, setTasks }: TaskItemProps) {
+export default function TaskItem({
+  setDraggedTask,
+  setDraggedTaskIndex,
+  task,
+  tasks,
+  index,
+  setTasks,
+}: TaskItemProps) {
   const [completeCheck, setCheck] = useState(false);
   const taskName = useRef(task.name);
 
   function taskComplete(complete: boolean) {
     const newTasks = [...tasks];
     newTasks.splice(index, 1, task);
-    setTasks(newTasks)
+    setTasks(newTasks);
     setCheck(complete);
   }
 
   // Add update change to DB to fix the name of the task
   function confirmTask(e) {
-    if(e.code == "Enter") {
+    if (e.code == "Enter") {
       try {
-        const newTasks = [...tasks]
-        task.name = taskName.current.value
+        const newTasks = [...tasks];
+        task.name = taskName.current.value;
         newTasks.splice(index, 1, task);
 
-        // const response = fetch("/api/addTask", {
-        //   method: "POST",
-        //   body: JSON.stringify(newTasks)
-        // })
+        fetch("/api/addTask", {
+          method: "POST",
+          body: JSON.stringify(task),
+        });
 
-        console.log(response)
-
-        setTasks(newTasks)
-      } catch(err) {
-        console.log("We have an error")
+        setTasks(newTasks);
+      } catch (err) {
+        console.log("We have an error");
         console.error(err);
       }
     }
@@ -47,10 +52,7 @@ export default function TaskItem({ setDraggedTask, setDraggedTaskIndex, task, ta
 
   function onTaskDrop(e: React.DragEvent<HTMLDivElement>) {
     e.preventDefault();
-    const dropIndex = Number.parseInt(
-      e.dataTransfer.getData("text/plain"),
-      10,
-    );
+    const dropIndex = Number.parseInt(e.dataTransfer.getData("text/plain"), 10);
     if (dropIndex != index) {
       const newTasks = [...tasks];
       const [movedTask] = newTasks.splice(dropIndex, 1);
@@ -88,14 +90,24 @@ export default function TaskItem({ setDraggedTask, setDraggedTaskIndex, task, ta
         className="w-3 h-3 border border-dashed border-gray-300 rounded-full flex-shrink-0"
       />
       <div className="flex-grow min-w-0">
-        <input className={`text-sm font-medium text-gray-900 truncate ${completeCheck ? 'line-through' : ''}`} onKeyDown={confirmTask} placeholder="New Task" defaultValue={task.name} ref={taskName}  />
+        <input
+          className={`text-sm font-medium text-gray-900 truncate ${
+            completeCheck ? "line-through" : ""
+          }`}
+          onKeyDown={confirmTask}
+          placeholder="New Task"
+          defaultValue={task.name}
+          ref={taskName}
+        />
         <div className="flex items-center text-xs text-gray-500 mt-0.5">
           <span>{formatTime(task.startTime.getHours())}</span>
           <span className="mx-1">-</span>
-          <span>{formatTime(task.startTime.getHours() + (task.Duration/60))}</span>
+          <span>
+            {formatTime(task.startTime.getHours() + task.Duration / 60)}
+          </span>
         </div>
       </div>
       <hr className="my-12 h-0.5 border-t-0 bg-amber-400" />
     </div>
-  )
+  );
 }
