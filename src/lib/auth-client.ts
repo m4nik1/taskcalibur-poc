ec3a30553dd7103a23395fd07c0cc2cc683a64c5
m4nik1 <maniksoomro@hotmail.com>
2025-09-01 22:17:35 +0100

    Added env var for known auth hosts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_AUTH_URL ||
+    "http://localhost:3000/api/auth", // fallback for dev
 })
